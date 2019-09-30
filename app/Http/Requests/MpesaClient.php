d85a93dfc46bc81cb2d275764122355e096344d0
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-30 15:10:04 +0300

    check initator

--- app/Http/Requests/MpesaClient.php
+++ app/Http/Requests/MpesaClient.php
@@ -16,7 +16,7 @@ class MpesaClient
         try{
             $mpesa = new \Safaricom\Mpesa\Mpesa();
 
-            $initiatorName=env('MPESA_B2C_INITIATOR_NAME');
+
             Log::debug('initiator'.$initiatorName);
             $securityCredential=self::getSecurityCredentials(false);
             $commandID=env("MPESA_B2C_COMMANDID");
@@ -27,6 +27,7 @@ class MpesaClient
             $occasion=$data->occasion;
             $queueTimeOutURL=env('MPESA_B2C_QUEUETIMEOUT_URL');
             $resultURL=env('MPESA_B2C_RESULT_URL');
+            $initiatorName=env('MPESA_B2C_INITIATOR_NAME'.$partyA);
             $b2cTransaction=$mpesa->b2c($initiatorName, $securityCredential, $commandID, $amount, $partyA, $partyB, $remarks, $queueTimeOutURL, $resultURL, $occasion);
             return $b2cTransaction;
 
