0b4fd3a71697789d277eb2459c4e5093b4c40fe4
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-30 16:35:41 +0300

    add check for env mode

--- app/Http/Requests/MpesaClient.php
+++ app/Http/Requests/MpesaClient.php
@@ -44,7 +44,7 @@ class MpesaClient
      */
     static function getSecurityCredentials(){
         $envMode =\env('MPESA_ENV');
-		($envMode=='sandbox') ? $fopen=fopen(storage_path("certs/sandbox.cer"),"r")
+        ($envMode=='sandbox') ? $fopen=fopen(storage_path("certs/cert.cer"),"r")
             : $fopen=fopen(storage_path("certs/production.cer"),"r");
 
 		$pub_key=fread($fopen,8192);
