8148ce42c66e495269902f3acf2a8677a4c99c51
Lee Mbuuga <mbugua.dev@gmail.com>
2019-10-03 22:18:18 +0300

    check b2c result !

--- app/Jobs/ProcessDisbursement.php
+++ app/Jobs/ProcessDisbursement.php
@@ -38,6 +38,7 @@ class ProcessDisbursement implements ShouldQueue
         Log::info('Processing new deisbursment request >>'.json_encode($this->disbursement));
         $b2c=MpesaClient::b2cPaymentRequest((object) $this->disbursement);
         if($b2c){
+            Log::debug('b2c >>>>>'.$b2c);
             $notification=['converstation_id'=>$b2c['ConversationID'],'originator'=>$b2c->OriginatorConversationID,'disb_reference'=>$this->disbursement->reference];
            $notify= DisbursementNotification::create($notification);
            $notify->save();
