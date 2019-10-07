5268f81e6a438099ca77822d7d00dd9a54d0e261
Lee Mbuuga <mbugua.dev@gmail.com>
2019-10-07 07:46:53 +0300

    update notifications

--- app/Jobs/ProcessDisbursement.php
+++ app/Jobs/ProcessDisbursement.php
@@ -38,7 +38,7 @@ class ProcessDisbursement implements ShouldQueue
         Log::info('Processing new deisbursment request >>'.json_encode($this->disbursement));
         $b2c=MpesaClient::b2cPaymentRequest((object) $this->disbursement);
         if($b2c){
-            $notificationData=\json_decode(json_encode($b2c,JSON_OBJECT_AS_ARRAY),true);
+            $notificationData=\json_encode($b2c,JSON_FORCE_OBJECT);
             // Log::info('disbursement response >>'.$b2c);
             Log::info('disbursement response >>'.$notificationData);
             // $notification=['converstation_id'=>$notificationData['ConversationID'],'originator'=>$notificationData['OriginatorConversationID'],'disb_reference'=>$this->disbursement->reference];
