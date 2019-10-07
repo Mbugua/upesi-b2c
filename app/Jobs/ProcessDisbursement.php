ce5c9a26d0290031ecd4e5c911e6fae6cfea0c7e
Lee Mbuuga <mbugua.dev@gmail.com>
2019-10-07 08:35:16 +0300

    check notification payload

--- app/Jobs/ProcessDisbursement.php
+++ app/Jobs/ProcessDisbursement.php
@@ -38,7 +38,7 @@ class ProcessDisbursement implements ShouldQueue
         Log::info('Processing new deisbursment request >>'.json_encode($this->disbursement));
         $b2c=MpesaClient::b2cPaymentRequest((object) $this->disbursement);
         if($b2c){
-            $notificationData=json_decode($b2c,true,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+            $notificationData=json_decode(json_decode($b2c,true,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
             Log::info('disbursement response >>'.$notificationData);
             \var_dump($notificationData);
             // Log::info('disbursement response >>'.$notificationData->ConversationID);
